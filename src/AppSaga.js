cbec0fb4aee173addde35e2ce15e252698defb03
ebenpack <ebenpackwood@gmail.com>
2018-02-25 11:48:01 -0500

    Add race example

--- src/AppSaga.js
+++ src/AppSaga.js
@@ -1,17 +1,24 @@
-import { call, takeEvery, put, select, all } from 'redux-saga/effects';
-import { ASYNC_INCREMENT, ASYNC_DECREMENT, increment, decrement } from './AppModule';
+import { call, race, take, takeEvery, put, select, all } from 'redux-saga/effects';
+import { ASYNC_INCREMENT, ASYNC_DECREMENT, INCREMENT, DECREMENT, increment, decrement } from './AppModule';
 
 function fakeHTTPRequest(delay) {
     return new Promise(
         resolve =>
-            setTimeout(resolve, delay)
+            setTimeout(() => resolve(true), delay)
     );
 }
 
 function* decrementSaga() {
     const delay = yield select(state => state.get('delay'));
-    const result = yield call(fakeHTTPRequest, delay);
-    yield put(decrement());
+    // 'Cancel' the async decrement if someone
+    // makes a sync decrement in the meantime
+    const { sync, async } = yield race({
+        async: call(fakeHTTPRequest, delay),
+        sync: take(DECREMENT)
+    });
+    if (async) {
+        yield put(decrement());
+    }
 }
 
 function* incrementSaga() {
